9597807fe09648cb3d12803b14517c4eb8bff43f
Anas Lamaiz <lamaizanass@gmail.com>
2022-10-15 01:43:32 +0100

    added controller

---
+++ src/main/java/com/example/springjenkins/MyController.java
@@ -0,0 +1,16 @@
+package com.example.springjenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello " + name;
+    }
+}
